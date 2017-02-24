df88861f1c487ebc6e33e849b5867ff8fe2f3d81
lecomte <jean-francois.lecomte@ifpen.fr>
2017-02-24 17:11:48 +0100

    handle Property update using Warp.
    Change-Id: I5cded435baaada4d5e41cf69042b3fc28b15fd8c

--- org.ifpen.stratiFX/src/stratifx/application/properties/PorosityComputer.java
+++ org.ifpen.stratiFX/src/stratifx/application/properties/PorosityComputer.java
@@ -3,6 +3,8 @@ package stratifx.application.properties;
 import fr.ifp.jdeform.decompaction.Porosity;
 import fr.ifp.kronosflow.controllers.property.PropertyComputer;
 import fr.ifp.kronosflow.geometry.Point2D;
+import fr.ifp.kronosflow.geoscheduler.GeoschedulerSection;
+import fr.ifp.kronosflow.geoscheduler.property.GeoschedulerPropertyComputer;
 import fr.ifp.kronosflow.mesh.IMeshProvider;
 import fr.ifp.kronosflow.mesh.Mesh2D;
 import fr.ifp.kronosflow.model.Patch;
@@ -21,124 +23,106 @@ import fr.ifp.kronosflow.property.PropertyInfo.Kind;
 import fr.ifp.kronosflow.property.PropertyInfo.Support;
 import fr.ifp.kronosflow.property.PropertyLocation;
 import fr.ifp.kronosflow.uids.UID;
+import fr.ifp.kronosflow.utils.LOGGER;
 
-public class PorosityComputer extends PropertyComputer {
-	
-	
-	Porosity porosity;
-	
-	
-	protected PorosityComputer(Section section) {
-		super(section);
-		PatchLibrary patchLib = section.getPatchLibrary();
-		porosity = new Porosity( patchLib.getPaleobathymetry() );
-		
-	}
-	
-	static public class Builder implements PropertyComputer.Builder {
-		@Override
-		public PropertyComputer create( Section section ) {
-			return new PorosityComputer(section);
-		}
-	}
-	
-	@Override
-	public Property compute() {
-		PatchLibrary patchLib = section.getPatchLibrary();
-		
-		PropertyDB propertyDB = section.getPropertyDB();
-		
-		PropertyInfo pinfo =  new PropertyInfo( "Porosity", Support.NodeProperty, Kind.Real );
-		
-		Property surfaceProp = propertyDB.findProperty( pinfo );
-		if ( null == surfaceProp ){
-			surfaceProp = propertyDB.createProperty(pinfo);
-		}
-		
-		IPropertyAccessor accessor = surfaceProp.getAccessor();
-		
-		for( Patch patch : patchLib.getPatches() ){
-			if ( patch instanceof IMeshProvider ){
-				computeUsingMesh( patch, accessor );
-			}
-			else {
-				computeUsingPatch( patch, accessor );
-			}
-		}
-		
-		PropertyStyle propStyle = new PropertyStyle(section.getStyle());
-		propStyle.setCurrent(surfaceProp);
-		
-		
-		return surfaceProp;
-		
-	}
-	
-	
-	private void computeUsingPatch( Patch patch, IPropertyAccessor accessor ) {
-		
-		
-		PolyLine border = patch.getBorder();
-	
-		for( ICurviPoint cp : border.getPoints() ){
-			
-			Point2D xy = border.getPosition(cp);
-			
-			double phi = porosity.getPorosity( xy.getPosition() );
-			
-			PropertyLocation location = new PropertyLocation( patch, xy.getPosition() );
-			
-			accessor.setValue( location,  phi );	
-		}
-		
-		
-	}
+public class PorosityComputer extends GeoschedulerPropertyComputer {
 
+    Porosity porosity;
 
-	private void computeUsingMesh( Patch patch, IPropertyAccessor accessor ) {	
-		
-		Mesh2D mesh = ((IMeshProvider)patch).getMesh();
-		IGeometryProvider provider = mesh.getGeometryProvider();
-		
-		for( UID uid : mesh.getNodeIds() ){
-			double phi = porosity.getPorosity( provider.getPosition(uid) );		
-			Node node = (Node)mesh.getNode(uid);
-			PropertyLocation location =  node.getLocation( patch );
-			accessor.setValue( location,  phi );	
-		}
-	}
+    protected PorosityComputer(GeoschedulerSection section) {
+        super(section);
+        PatchLibrary patchLib = section.getPatchLibrary();
+        porosity = new Porosity(patchLib.getPaleobathymetry());
 
+    }
 
-	@Override
-	public Property compute(Patch patchToCompute) {
-		PropertyDB propertyDB = section.getPropertyDB();
-		
-		
-		
-		PropertyInfo pinfo =  new PropertyInfo( "Porosity", Support.NodeProperty, Kind.Real );
-		
-		Property surfaceProp = propertyDB.findProperty( pinfo );
-		if ( null == surfaceProp ){
-			surfaceProp = propertyDB.createProperty(pinfo);
-		}
-		
-		IPropertyAccessor accessor = surfaceProp.getAccessor();
-		
-			if ( patchToCompute instanceof IMeshProvider ){
-				computeUsingMesh( patchToCompute, accessor );
-			}
-			else {
-				computeUsingPatch( patchToCompute, accessor );
-			}
-		
-		
-		PropertyStyle propStyle = new PropertyStyle(section.getStyle());
-		propStyle.setCurrent(surfaceProp);
-		
-		
-		return surfaceProp;
-	}
-	
+    static public class Builder implements PropertyComputer.Builder {
 
+        @Override
+        public PropertyComputer create(Section section) {
+            if (section instanceof GeoschedulerSection) {
+                return new PorosityComputer((GeoschedulerSection) section);
+            }
+
+            LOGGER.error("need a GeoschedulerSection to create this PropertyComputer", getClass());
+            return null;
+        }
+    }
+
+    @Override
+    public Property compute() {
+        PatchLibrary patchLib = section_.getPatchLibrary();
+
+        PropertyInfo pinfo = new PropertyInfo("Porosity", Support.NodeProperty, Kind.Real);
+
+        Property surfaceProp = findOrCreateProperty(pinfo);
+        
+        IPropertyAccessor accessor = surfaceProp.getAccessor();
+
+        for (Patch patch : patchLib.getPatches()) {
+            if (patch instanceof IMeshProvider) {
+                computeUsingMesh(patch, accessor);
+            } else {
+                computeUsingPatch(patch, accessor);
+            }
+        }
+
+        PropertyStyle propStyle = new PropertyStyle(section_.getStyle());
+        propStyle.setCurrent(surfaceProp);
+
+        return surfaceProp;
+
+    }
+
+    private void computeUsingPatch(Patch patch, IPropertyAccessor accessor) {
+
+        PolyLine border = patch.getBorder();
+
+        for (ICurviPoint cp : border.getPoints()) {
+
+            Point2D xy = border.getPosition(cp);
+
+            double phi = porosity.getPorosity(xy.getPosition());
+
+            PropertyLocation location = new PropertyLocation(patch, xy.getPosition());
+
+            accessor.setValue(location, phi);
+        }
+
+    }
+
+    private void computeUsingMesh(Patch patch, IPropertyAccessor accessor) {
+
+        Mesh2D mesh = ((IMeshProvider) patch).getMesh();
+        IGeometryProvider provider = mesh.getGeometryProvider();
+
+        for (UID uid : mesh.getNodeIds()) {
+            double phi = porosity.getPorosity(provider.getPosition(uid));
+            Node node = (Node) mesh.getNode(uid);
+            PropertyLocation location = node.getLocation(patch);
+            accessor.setValue(location, phi);
+        }
+    }
+
+    @Override
+    public Property compute(Patch patchToCompute) {
+        
+        PropertyInfo pinfo = new PropertyInfo("Porosity", Support.NodeProperty, Kind.Real);
+
+        Property surfaceProp = findOrCreateProperty(pinfo);
+        
+        IPropertyAccessor accessor = surfaceProp.getAccessor();
+
+        if (patchToCompute instanceof IMeshProvider) {
+            computeUsingMesh(patchToCompute, accessor);
+        } else {
+            computeUsingPatch(patchToCompute, accessor);
+        }
+
+        PropertyStyle propStyle = new PropertyStyle(section_.getStyle());
+        propStyle.setCurrent(surfaceProp);
+
+        return surfaceProp;
+    }
 
 }
--- org.ifpen.stratiFX/src/stratifx/application/properties/XYPropertyComputer.java
+++ org.ifpen.stratiFX/src/stratifx/application/properties/XYPropertyComputer.java
@@ -2,6 +2,8 @@ package stratifx.application.properties;
 
 import fr.ifp.kronosflow.controllers.property.PropertyComputer;
 import fr.ifp.kronosflow.geometry.Point2D;
+import fr.ifp.kronosflow.geoscheduler.property.GeoschedulerPropertyComputer;
+import fr.ifp.kronosflow.geoscheduler.GeoschedulerSection;
 import fr.ifp.kronosflow.mesh.IMeshProvider;
 import fr.ifp.kronosflow.mesh.Mesh2D;
 import fr.ifp.kronosflow.model.Patch;
@@ -13,117 +15,103 @@ import fr.ifp.kronosflow.polyline.Node;
 import fr.ifp.kronosflow.polyline.PolyLine;
 import fr.ifp.kronosflow.property.IPropertyAccessor;
 import fr.ifp.kronosflow.property.Property;
-import fr.ifp.kronosflow.property.PropertyDB;
 import fr.ifp.kronosflow.property.PropertyInfo;
 import fr.ifp.kronosflow.property.PropertyInfo.Kind;
 import fr.ifp.kronosflow.property.PropertyInfo.Support;
 import fr.ifp.kronosflow.property.PropertyLocation;
 import fr.ifp.kronosflow.uids.UID;
+import fr.ifp.kronosflow.utils.LOGGER;
 
-public class XYPropertyComputer extends PropertyComputer {
+public class XYPropertyComputer extends GeoschedulerPropertyComputer {
 
-	protected XYPropertyComputer(Section section) {
-		super(section);
-	}
-	
-	static public class Builder implements PropertyComputer.Builder {
-		@Override
-		public PropertyComputer create( Section section ) {
-			return new XYPropertyComputer(section);
-		}
-	}
-	
-	@Override
-	public Property compute() {
-		PatchLibrary patchLib = section.getPatchLibrary();
-		
-		PropertyDB propertyDB = section.getPropertyDB();
-		
-		PropertyInfo pinfo =  new PropertyInfo( "XY", Support.NodeProperty, Kind.Vector );
-		
-		Property surfaceProp = propertyDB.findProperty( pinfo );
-		if ( null == surfaceProp ){
-			surfaceProp = propertyDB.createProperty(pinfo);
-		}
-		
-		IPropertyAccessor accessor = surfaceProp.getAccessor();
-		
-		for( Patch patch : patchLib.getPatches() ){
-			if ( patch instanceof IMeshProvider ){
-				computeUsingMesh( patch, accessor );
-			}
-			else {
-				computeUsingPatch( patch, accessor );
-			}
-		}
-		
-		PropertyStyle propStyle = new PropertyStyle(section.getStyle());
-		propStyle.setCurrent(surfaceProp);
-		
-		
-		return surfaceProp;
-		
-	}
-	
-	
-	private void computeUsingPatch( Patch patch, IPropertyAccessor accessor ) {
-		
-		PolyLine border = patch.getBorder();
-	
-		for( ICurviPoint cp : border.getPoints() ){
-			Point2D pt = border.getPosition(cp);
-			
-			PropertyLocation location = new PropertyLocation( patch, pt.getPosition() );
-			accessor.setValue( location, pt.getPosition() );
-		}
-		
-		
-	}
+    protected XYPropertyComputer(GeoschedulerSection section) {
+        super(section);
+    }
 
+    static public class Builder implements PropertyComputer.Builder {
 
-	private void computeUsingMesh( Patch patch, IPropertyAccessor accessor ) {	
-		
-		
-		Mesh2D mesh = ((IMeshProvider)patch).getMesh();
-			
-		for( UID uid : mesh.getNodeIds() ){
-			Node node = (Node)mesh.getNode(uid);
-			PropertyLocation location = node.getLocation( patch );
-			accessor.setValue( location,  node.getPosition()  );	
-		}
-		
-	}
+        @Override
+        public PropertyComputer create(Section section) {
 
+            if (section instanceof GeoschedulerSection) {
+                return new XYPropertyComputer((GeoschedulerSection) section);
+            }
 
-	@Override
-	public Property compute(Patch patchToCompute) {
-		
-		PropertyDB propertyDB = section.getPropertyDB();
-		
-		PropertyInfo pinfo =  new PropertyInfo( "XY", Support.NodeProperty, Kind.Vector );
-		
-		Property surfaceProp = propertyDB.findProperty( pinfo );
-		if ( null == surfaceProp ){
-			surfaceProp = propertyDB.createProperty(pinfo);
-		}
-		
-		IPropertyAccessor accessor = surfaceProp.getAccessor();
-		
-		if ( patchToCompute instanceof IMeshProvider ){
-			computeUsingMesh( patchToCompute, accessor );
-		}
-		else {
-			computeUsingPatch( patchToCompute, accessor );
-		}
-		
-		
-		PropertyStyle propStyle = new PropertyStyle(section.getStyle());
-		propStyle.setCurrent(surfaceProp);
-		
-		
-		return surfaceProp;
-	}
-	
+            LOGGER.error("need a GeoschedulerSection to create this PropertyComputer", getClass());
 
+            return null;
+        }
+    }
+
+    @Override
+    public Property compute() {
+
+        Property surfaceProp = findOrCreateProperty(
+                new PropertyInfo("XY", Support.NodeProperty, Kind.Vector)
+        );
+
+        IPropertyAccessor accessor = surfaceProp.getAccessor();
+
+        PatchLibrary patchLib = section_.getPatchLibrary();
+        for (Patch patch : patchLib.getPatches()) {
+            if (patch instanceof IMeshProvider) {
+                computeUsingMesh(patch, accessor);
+            } else {
+                computeUsingPatch(patch, accessor);
+            }
+        }
+
+        PropertyStyle propStyle = new PropertyStyle(section_.getStyle());
+        propStyle.setCurrent(surfaceProp);
+
+        return surfaceProp;
+
+    }
+
+    private void computeUsingPatch(Patch patch, IPropertyAccessor accessor) {
+
+        PolyLine border = patch.getBorder();
+
+        for (ICurviPoint cp : border.getPoints()) {
+            Point2D pt = border.getPosition(cp);
+
+            PropertyLocation location = new PropertyLocation(patch, pt.getPosition());
+            accessor.setValue(location, pt.getPosition());
+        }
+
+    }
+
+    private void computeUsingMesh(Patch patch, IPropertyAccessor accessor) {
+
+        Mesh2D mesh = ((IMeshProvider) patch).getMesh();
+
+        for (UID uid : mesh.getNodeIds()) {
+            Node node = (Node) mesh.getNode(uid);
+            PropertyLocation location = node.getLocation(patch);
+            accessor.setValue(location, node.getPosition());
+        }
+
+    }
+
+    @Override
+    public Property compute(Patch patchToCompute) {
+
+        Property surfaceProp = findOrCreateProperty(
+                new PropertyInfo("XY", Support.NodeProperty, Kind.Vector)
+            );
+
+        IPropertyAccessor accessor = surfaceProp.getAccessor();
+
+        if (patchToCompute instanceof IMeshProvider) {
+            computeUsingMesh(patchToCompute, accessor);
+        } else {
+            computeUsingPatch(patchToCompute, accessor);
+        }
+
+        PropertyStyle propStyle = new PropertyStyle(section_.getStyle());
+        propStyle.setCurrent(surfaceProp);
+
+        return surfaceProp;
+    }
 
 }
